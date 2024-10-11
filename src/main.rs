a9590c69d3ec8327fb8b09f958a38175a9655c77
Evan Simmons <esims89@gmail.com>
2024-10-11 10:43:02 -0700

    deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use tracing::info;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(env = "CHAOS_RUNNER", short, long)]
+    runner: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let cli = Cli::parse();
+    info!("cli: {cli:?}");
 }
